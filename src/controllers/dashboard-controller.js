d4226d8a3bc0acd9105d4507c7fafa24695b53f9
Fiona Waters <fiwaters6@gmail.com>
2022-03-05 10:02:36 +0000

    joi form validation added, json model added

--- src/controllers/dashboard-controller.js
+++ src/controllers/dashboard-controller.js
@@ -1,4 +1,5 @@
 import { db } from "../models/db.js";
+import { CraftSpec } from "../models/joi-schemas.js";
 
 export const dashboardController = {
   index: {
@@ -15,6 +16,13 @@ export const dashboardController = {
   },
 
   addCraft: {
+    validate: {
+      payload: CraftSpec,
+      options: { abortEarly: false },
+      failAction: function (request, h, error) {
+        return h.view("dashboard-view", { title: "Error Adding Craft", errors: error.details }).takeover().code(400);
+      },
+    },
     handler: async function (request, h) {
       const loggedInUser = request.auth.credentials;
       const newCraft = {
