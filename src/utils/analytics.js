32f4b600fcd30982cb2b0b165e59368048951c78
Fiona Waters <fiwaters6@gmail.com>
2022-03-21 12:54:27 +0000

    working on analytics

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -6,10 +6,10 @@ async calculateAnalytics() {
     const averageCraftsPerUser = await analytics.averageCraftsPerUser();
     const averageSpotsPerUser = await analytics.averageSpotsPerUser();
     const averageSpotsPerCraft = await analytics.averageSpotsPerCraft();
-    // const leastCrafts = await analytics.leastCrafts();
-    //  const mostCrafts = await analytics.mostCrafts();
+    const leastCrafts = await analytics.leastCrafts();
+    const mostCrafts = await analytics.mostCrafts();
 
-    return {averageCraftsPerUser, averageSpotsPerUser, averageSpotsPerCraft};
+    return {averageCraftsPerUser, averageSpotsPerUser, averageSpotsPerCraft, leastCrafts, mostCrafts};
 },
 
   async averageCraftsPerUser() {
@@ -32,18 +32,33 @@ async calculateAnalytics() {
 
   async leastCrafts() {
     let minValue = 0;
-    const users = await db.users.getAllUsers();
+    const users = await db.userStore.getAllUsers();
     for (let i = 0; i < users.length; i += 1) {
       // eslint-disable-next-line no-await-in-loop
       const userCrafts = await db.craftStore.getUserCrafts(users[i]._id);
+      if(userCrafts) {
+      minValue = userCrafts.length;
       if (userCrafts.length < minValue) {
         minValue = userCrafts.length;
       }
     }
+  }
     return minValue;
   },
 
-// most crafts
+  async mostCrafts() {
+    let maxValue = 0;
+    const users = await db.userStore.getAllUsers();
+    for (let i = 0; i < users.length; i += 1) {
+      // eslint-disable-next-line no-await-in-loop
+      const userCrafts = await db.craftStore.getUserCrafts(users[i]._id);
+      maxValue = userCrafts.length;
+      if (userCrafts.length > maxValue) {
+          maxValue = userCrafts.length;
+      }
+  }
+    return maxValue;
+  },
 
 };
 
